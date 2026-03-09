3616c25d9843004f6e4fe37e8253a6fbd4f4d26d
Ind028 <b230011@nitsikkim.ac.in>
2026-03-09 11:00:37 +0530

    Add files via upload

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,13 +1,15 @@
 def encrypt(text, s):
     result = ""
+    a=1
     for i in range(len(text)):
         char = text[i]
         if (char.isupper()):
-            result += chr((ord(char) + s - 65) % 26 + 65)
+            result += chr((a*(ord(char)) + s - 65) % 26 + 65)
         elif not char.isalpha():
             result += char
         else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
+            result += chr((a*(ord(char))indrani
+                            + s - 97) % 26 + 97)
     return result
 
 text = input("Enter the message: ") 
@@ -16,3 +18,8 @@ print("Text  : " + text)
 print("Shift : " + str(s))
 print("Cipher: " + encrypt(text, s))
 #classic Caesar cipher, digits, numbers, spaces, and punctuation were left unencrypted.
+#The Affine Cipher formula is:(a*char+b)mod26
+#Ceaser Cipher formula:(char+b)mod26
+#Special case meaning: a=1 (substisute in affine cipher and you get ceaser cipher)
+
+
