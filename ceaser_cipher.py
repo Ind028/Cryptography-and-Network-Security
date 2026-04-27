f7d2bb4d4d4be6eeb89c3491d824f944ee054639
INDRANI ADHIKARY <182662459+Ind028@users.noreply.github.com>
2026-04-27 11:25:06 +0530

    Update Caesar cipher with encryption and decryption functions
    Refactor Caesar cipher implementation to include decryption and fix syntax errors.

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,22 +1,33 @@
-def encrypt(text, s):
-    result = ""
-    a=1
-    for i in range(len(text)):
-        char = text[i]
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def enc(text,s):
+    res=""
+    for i in range (len(text)):
+        char=text[i]
         if (char.isupper()):
-            result += chr((a*(ord(char)) + s - 65) % 26 + 65)
-        elif not char.isalpha():
-            result += char
+            res+=chr((ord(char)-65+s)%26+65)
+        elif(char.islower()):
+            res+=chr((ord(char)-97+s)%26+97)
         else:
-            result += chr((a*(ord(char))indrani
-                            + s - 97) % 26 + 97)
-    return result
+            res+=char
+    return res
+def dec(text,s):
+    res2=""
+    for i in range (len(text)):
+        char=text[i]
+        if (char.isupper()):
+            res2+=chr((ord(char)-65-s)%26+65)
+        elif (char.islower()):
+            res2+=chr((ord(char)-97-s)%26+97)
+        else:
+            res2+=char
+    return res2
+text=input("Enter the message:")
+s=int(input("Enter the value of shift:"))
+res3=enc(text,s)
+print("Encrypted message:",res3)
+print("Dencrypted message:",dec(res3,s))
 
-text = input("Enter the message: ") 
-s = int(input("Enter shift value: "))
-print("Text  : " + text)
-print("Shift : " + str(s))
-print("Cipher: " + encrypt(text, s))
 #classic Caesar cipher, digits, numbers, spaces, and punctuation were left unencrypted.
 #The Affine Cipher formula is:(a*char+b)mod26
 #Ceaser Cipher formula:(char+b)mod26
