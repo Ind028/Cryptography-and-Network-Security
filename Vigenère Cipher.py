bc80dd2ed71789ba2279b60373e8b79c2ac20025
INDRANI ADHIKARY <182662459+Ind028@users.noreply.github.com>
2026-04-27 12:07:09 +0530

    Refactor Vigenère cipher functions
    Refactored the Vigenère cipher functions for encryption and decryption with new function names and improved variable naming.

--- Vigenère Cipher.py
+++ Vigenère Cipher.py
@@ -1,49 +1,37 @@
 # Caesar cipher function (used as component)
-def vigenere_encrypt(text, key):
-    result = ""
-    key_index = 0
-
+def vig_enc(text,key):
+    res=""
+    key_index=0
     for char in text:
         if char.isalpha():
-            shift = ord(key[key_index % len(key)].lower()) - ord('a')
-
+            shift=ord(key[key_index%len(key)].lower())-97
             if char.isupper():
-                result += chr((ord(char) - 65 + shift) % 26 + 65)
+                res+=chr((ord(char)-65+shift)%26+65)
             else:
-                result += chr((ord(char) - 97 + shift) % 26 + 97)
-
-            key_index += 1
+                res+=chr((ord(char)-97+shift)%26+97)
+            key_index+=1
         else:
-            result += char
-
-    return result
-
-
-def vigenere_decrypt(text, key):
-    result = ""
-    key_index = 0
-
+            res+=char
+    return res
+def vig_dec(text,key):
+    res2=""
+    key_index=0
     for char in text:
         if char.isalpha():
-            shift = ord(key[key_index % len(key)].lower()) - ord('a')
-
+            shift2=ord(key[key_index%len(key)].lower())-97
             if char.isupper():
-                result += chr((ord(char) - 65 - shift) % 26 + 65)
+                res2+=chr((ord(char)-65-shift2)%26+65)
             else:
-                result += chr((ord(char) - 97 - shift) % 26 + 97)
-
-            key_index += 1
+                res2+=chr((ord(char)-97-shift2)%26+97)
+            key_index+=1
         else:
-            result += char
-
-    return result
-
-
-text = input("Enter the message: ")
-key = input("Enter the key: ")
-
-encrypted = vigenere_encrypt(text, key)
-decrypted = vigenere_decrypt(encrypted, key)
-
+            res2+=char
+    return res2
+text=input("Enter the text:")
+key=input("Enter the key:")
+res3=vig_enc(text,key)
+print("Encrypted msg:",res3)
+print("dEncrypted msg:",vig_dec(res3,key))
+        
 print("Encrypted:", encrypted)
 print("Decrypted:", decrypted)
